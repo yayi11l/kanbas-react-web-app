ca9e8e447ce6c48fdd345218e16ce3cb289dd3a1
yayi <yayiaimeeluo@gmail.com>
2024-06-16 20:53:43 +0800

    Lab3

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,79 @@
+import Add from "./Add";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import ArrowFunctions from "./ArrowFunctions";
+import BooleanVariables from "./BooleanVariables";
+import Classes from "./Classes";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import Destructing from "./Destructing";
+import DestructingImports from "./DestructingImports";
+import FilterFunction from "./FilterFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import ForLoops from "./ForLoops";
+import FunctionDestructing from "./FunctionDestructing";
+import Highlight from "./Highlight";
+import House from "./House";
+import IfElse from "./IfElse";
+import ImpliedReturn from "./ImpliedReturn";
+import JsonStringify from "./JsonStringify";
+import LegacyFunctions from "./LegacyFunctions";
+import MapFunction from "./MapFunction";
+import PathParameters from "./PathParameters";
+import SimpleArrays from "./SimpleArrays";
+import Spreading from "./Spreading";
+import Square from "./Square";
+import Styles from "./Styles";
+import TemplateLiterals from "./TemplateLiterals";
+import TernaryOperator from "./TernaryOperator";
+import TodoList from "./TodoList";
+import VariableTypes from "./VariableTypes";
+import VariablesAndConstants from "./VariablesAndConstants";
+
 export default function Lab3() {
+  console.log('Hello World!');
   return (
-    <div>
+    <div id="wd-lab3" className="container-fluid">
       <h2>Lab 3</h2>
+      <VariablesAndConstants />
+      <VariableTypes />
+      <BooleanVariables />
+      <IfElse />
+      <TernaryOperator />
+      <ConditionalOutputIfElse />
+      <ConditionalOutputInline />
+      <LegacyFunctions />
+      <ArrowFunctions />
+      <ImpliedReturn />
+      <TemplateLiterals />
+      <SimpleArrays />
+      <ArrayIndexAndLength />
+      <AddingAndRemovingToFromArrays />
+      <ForLoops />
+      <MapFunction />
+      <FindFunction />
+      <FindIndex />
+      <FilterFunction />
+      <JsonStringify />
+      <House />
+      <TodoList />
+      <Spreading />
+      <Destructing />
+      <FunctionDestructing />
+      <DestructingImports />
+      <Classes />
+      <Styles />
+      <Add a={4} b={3} />
+      <h4>Square of 4</h4>
+      <Square>4</Square>
+      <hr />
+      <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+      </Highlight>
+      <PathParameters />
+      
     </div>
   );
 }
